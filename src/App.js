5d8e117aaf2c6ed46baa5b843dd78ee49cae31ee
youngjae99 <youngjae.j99@gmail.com>
2021-08-06 02:01:23 +0900

    gpt2-dino model use

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, {useState} from 'react';
 import logo from "./logo.svg";
+import axios from 'axios';
 import "./App.css";
 import { Button, ButtonToolbar, Form, FormGroup } from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      let bodyFormData = new FormData();
+      bodyFormData.append('text', '내가 너를 많이 사랑해');
+      bodyFormData.append('num_samples', 20);
+      const response = await axios.post('https://main-ko-gpt2-dino-fpem123.endpoint.ainize.ai/gen', bodyFormData, {
+        headers: {
+          // Overwrite Axios's automatically set Content-Type
+          'Content-Type': 'multipart/form-data',
+          'accept': 'application/json'
+        }
+      });
+      setData(response.data);
+    } catch (e){
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
       <Form>
@@ -13,7 +34,13 @@ function App() {
             상대에게 톡하기 전에, 제 생각을 들려줄께요
           </Form.Text>
         </Form.Group>
+        <a onClick={onClick}
+          class="btn btn-lg btn-secondary fw-bold border-white bg-white">
+          판단해줘!
+        </a><br></br>
+        {data && <textarea rows={7} value={JSON.stringify(data, null, 2)}></textarea>}
       </Form>
+      <br/>
     </div>
   );
 }
