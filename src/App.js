1e41912bfdbdc2835f350982f5dd943aefa07205
youngjae99 <youngjae.j99@gmail.com>
2021-08-06 01:42:27 +0900

    Bootstrap이용 frame구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, ButtonToolbar, Form, FormGroup } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>어떤 말을 할 생각인가요?</Form.Label>
+          <Form.Control type="text" placeholder="한나야 자니..?" />
+          <Form.Text className="text-muted">
+            상대에게 톡하기 전에, 제 생각을 들려줄께요
+          </Form.Text>
+        </Form.Group>
+      </Form>
     </div>
   );
 }
