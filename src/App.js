bd0e755d208d075a08be786c51992adbc154a9fe
youngjae99 <youngjae.j99@gmail.com>
2021-08-06 19:55:43 +0900

    improvements: spinner added, bootstrap form to show results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import logo from "./logo.svg";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
-import { Button, ButtonToolbar, Form, FormGroup } from "react-bootstrap";
+import { Button, ButtonToolbar, Form, FormGroup, Spinner} from "react-bootstrap";
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [spinner, showSpinner] = useState(false);
+
   const onClick = async () => {
+    showSpinner(true);
     try {
       let bodyFormData = new FormData();
-      bodyFormData.append('text', '내가 너를 많이 사랑해');
-      bodyFormData.append('num_samples', 20);
-      const response = await axios.post('https://main-ko-gpt2-dino-fpem123.endpoint.ainize.ai/gen', bodyFormData, {
-        headers: {
-          // Overwrite Axios's automatically set Content-Type
-          'Content-Type': 'multipart/form-data',
-          'accept': 'application/json'
+      bodyFormData.append("text", "내가 너를 많이 사랑해");
+      bodyFormData.append("num_samples", 20);
+      const response = await axios.post(
+        "https://main-ko-gpt2-dino-fpem123.endpoint.ainize.ai/gen",
+        bodyFormData,
+        {
+          headers: {
+            // Overwrite Axios's automatically set Content-Type
+            "Content-Type": "multipart/form-data",
+            accept: "application/json",
+          },
         }
-      });
+      );
       setData(response.data);
-    } catch (e){
+      showSpinner(false);
+    } catch (e) {
       console.log(e);
     }
   };
@@ -34,15 +42,32 @@ const App = () => {
             상대에게 톡하기 전에, 제 생각을 들려줄께요
           </Form.Text>
         </Form.Group>
-        <a onClick={onClick}
-          class="btn btn-lg btn-secondary fw-bold border-white bg-white">
+        <a
+          onClick={onClick}
+          class="btn btn-lg btn-secondary fw-bold border-white bg-white"
+        >
           판단해줘!
-        </a><br></br>
-        {data && <textarea rows={7} value={JSON.stringify(data, null, 2)}></textarea>}
+        </a>
+        <br/>
+        {spinner && (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>)
+        }
+        {data && (
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>분석 결과</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={7}
+              value={JSON.stringify(data, null, 2)}
+            />
+          </Form.Group>
+        )}
       </Form>
-      <br/>
+      <br />
     </div>
   );
-}
+};
 
 export default App;
